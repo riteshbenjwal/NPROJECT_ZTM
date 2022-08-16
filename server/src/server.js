c1008bb4915e5ebd3110147d6f2a355631d2ac25
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-08-16 11:12:41 +0530

    PLANETS_ROUTER_CONTROLLER

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const app = require("./app");
+const { loadPlanetsData } = require('./models/planets.model');
+
+const PORT = process.env.PORT || 8000; // set PORT=5000 && node server.js on package json
+
+const server = http.createServer(app); // create a server using the app and using native http module
+
+
+async function startServer() {
+    await loadPlanetsData();
+    
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}...`);
+    });
+  }
+  
+startServer(); // start the server
