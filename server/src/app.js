3da9844d29387a20a3781c86219b424b47fd006d
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-08-16 17:20:20 +0530

    Serving_CLIENT_BUILD_FROM_SERVER

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const planetsRouter = require('./routes/planets/planets.router.js')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const planetsRouter = require("./routes/planets/planets.router.js");
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(planetsRouter);
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..', 'public','index.html'));
+});
 
 module.exports = app;
