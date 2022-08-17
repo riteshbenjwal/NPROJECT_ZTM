44929972cda2b8bf6e03d415c2ab0f4feac711cf
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-08-17 17:38:32 +0530

    Launches

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const morgan = require('morgan');
+
+
 const planetsRouter = require("./routes/planets/planets.router.js");
+const launchesRouter = require("./routes/launches/launches.router.js");
 
 const app = express();
 
@@ -10,12 +14,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 
-app.get('/',(req,res)=>{
+app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'..', 'public','index.html'));
 });
 
