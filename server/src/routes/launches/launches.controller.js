4516cc66e50377a67964190b8efbbc35b9562f0d
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-08-19 12:30:50 +0530

    ABORT_API_COMPLETED

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,8 @@
 const {
   getAllLaunches,
   addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
 } = require("../../models/launches.models");
 
 function httpGetAllLaunches(req, res) {
@@ -33,7 +35,24 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+  const launchId = +req.params.id;
+
+  // if launch doesn't exist
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  // if launch does exist
+  const aborted = abortLaunchById(launchId);
+
+  return res.status(200).json(aborted);
+}
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { httpGetAllLaunches, httpAddNewLaunch } = require('./launches.controller');
+const { httpGetAllLaunches, httpAddNewLaunch , httpAbortLaunch} = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
 launchesRouter.get('/launches',httpGetAllLaunches);
 launchesRouter.post('/launches',httpAddNewLaunch);
+launchesRouter.delete('/launches/:id',httpAbortLaunch);
 
 module.exports = launchesRouter;
